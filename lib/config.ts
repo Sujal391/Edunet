45cf4caef6bb474afea0d4b387833ee4f2be9c88
Sujal391 <modisujal31@gmail.com>
2026-04-07 17:03:25 +0530

    feat: add configuration constants for API endpoints and base URLs

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,8 @@
 export const API_BASE_URL =
     process.env.NEXT_PUBLIC_API_URL;
 
+export const API_PROXY_BASE = "/api/proxy";
+
 export const API_ENDPOINTS = {
     LOGIN: "/access/",
     REFRESH: "/refresh/",
